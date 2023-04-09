41609197f43be32cbf0f2cc61c851914b584995d
saiyam3214 <87685722+saiyam3214@users.noreply.github.com>
2023-04-09 11:19:55 +0530

    Add files via upload

--- createweb.sh
+++ createweb.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 touch index.html
+echo "<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    
+</body>
+</html>" > index.html
 touch style.css
+echo "*{
+    padding: 0px;
+    margin: 0px;
+}" > style.css
 touch script.js
 mkdir assets
-mkdir components
 
